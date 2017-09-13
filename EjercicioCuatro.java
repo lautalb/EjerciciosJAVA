1d278ecc960eaf4e4ef3c05e814606aafaabc725
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 17:44:57 -0300

    primer Commit
    ejercicio 4

---
+++ EjercicioCuatro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuatro;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioCuatro {
+
+    /**
+     * @param args the command line arguments
+     * Al ingresar una edad y el nombre se debe mostrar el siguiente mensaje:
+     * Usted se llama xxxx y tiene xx años de edad.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
