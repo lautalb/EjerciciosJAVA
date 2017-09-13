af97dc86c792ebcdd265f88d98934b7abedc1d5a
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 17:44:37 -0300

    primer Commit
    ejercicio 3

---
+++ EjercicioTres.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciotres;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioTres {
+
+    /**
+     * @param args the command line arguments
+     * se debe pedir el nombre y el apellido, luego mostrarlo en el siguiente mensaje
+     * su nombre es xxxx y xxxxx es su apellido
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
