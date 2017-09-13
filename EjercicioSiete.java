5540c68882012b7333262192a5e132b66f2b9938
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 17:52:05 -0300

    primer Commit
    ejercicio7

---
+++ EjercicioSiete.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosiete;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioSiete {
+
+    /**
+     * @param args the command line arguments
+     * se pide la base de un cuadrado y se informa el perimetro.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
