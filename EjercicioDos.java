cd31ba9a614b1794cb7f231f6a71bebf0f9a4417
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 16:02:45 -0300

    primer Comit
    se da el ejercicio para realizarlo

---
+++ EjercicioDos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciodos;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioDos {
+
+    /**
+     * @param args the command line arguments
+     * Se debe pedir un nombre y se debe mostrar el mismo nombre.
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
