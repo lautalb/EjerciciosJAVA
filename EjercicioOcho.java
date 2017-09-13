487909d40a289adf211c9acd3487cedc0a43b238
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 17:48:59 -0300

    primer Commit
    ejercicio 8

---
+++ EjercicioOcho.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioocho;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioOcho {
+
+    /**
+     * @param args the command line arguments
+     * se pide el largo y el ancho de un terreno y se informa la superfice del mismo.
+     * 
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
