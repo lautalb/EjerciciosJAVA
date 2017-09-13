8919d7d696f1e552925c8a91b0e9589665be06fa
lautalb <31898586+lautalb@users.noreply.github.com>
2017-09-13 17:46:44 -0300

    primer Commit
    ejercicio 6

---
+++ EjercicioSeis.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioseis;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioSeis {
+
+    /**
+     * @param args the command line arguments
+     * Se piden 3 numeros y se informa el promedio.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
