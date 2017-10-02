fd8659b2c370e359028e5220156701dbb0d74888
lautalb <31898586+lautalb@users.noreply.github.com>
2017-10-02 11:26:22 -0300

    segundo commit
    ejercicio comentado y terminado

--- EjercicioTreintaYCinco.java
+++ EjercicioTreintaYCinco.java
@@ -9,28 +9,34 @@ import java.util.Scanner;
 
 /**
  *
- * @author alumno
+ * @author Lautalb
  */
 public class EjercicioTreintaYCinco {
 
     /**
      * @param args the command line arguments
-     * 35- Las lámparas están al mismo precio de $35 pesos final. 
+     * Ferretería Ferrete
+Debemos hacer una serie de aplicaciones para este negocio
+	35- Las lámparas están al mismo precio de $35 pesos final. 
 A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%. 
 B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%. 
 C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%. 
 D. Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz" el descuento es del 15%, si es “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%. 
-E. Si el importe final con descuento suma más de $120 se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:”IIBB Usted pago X”, siendo X el impuesto que se pagó.
+E. Si el importe final con descuento suma más de $120 se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:”IIBB Usted pago X”, 
+* siendo X el impuesto que se pagó.
+
      */
     public static void main(String[] args) {
-        //declaramos las variables e importamos el scanner a utilizar
+         //declaramos las variables e importamos el scanner a utilizar
         Scanner miscan=new Scanner(System.in);
+        
         String auxCantidad;
         Integer cantidadDeLamparitas;
         Integer precioDeLamparita;
         String marcaDeLampara;
-        Integer totalBruto;
+        Double impuesto;
         Double precioTotal;
+        Double ingresoBrutoTotal;
         
         //Double precioFinal=(double)0; se castea para poder inciar en 0 una variable double
         //definimos el precio de la lamparita
@@ -45,20 +51,66 @@ E. Si el importe final con descuento suma más de $120 se debe sumar un 10% de i
         
         switch(cantidadDeLamparitas)
         {
+            case 5:
+                if (marcaDeLampara.equals("argentinaluz") )
+                {
+                    precioTotal=cantidadDeLamparitas*precioDeLamparita*0.60;
+                    System.out.println("debe pagar: "+precioTotal+"$.");
+                }else
+                {
+                    precioTotal=cantidadDeLamparitas*precioDeLamparita*0.70;
+                    impuesto=cantidadDeLamparitas*precioDeLamparita*0.10;
+                    ingresoBrutoTotal=precioTotal+impuesto;
+                    
+                    System.out.println("IIBB usted pago: "+ingresoBrutoTotal+"$, siendo "+impuesto+"$ el impuesto que se pagó.");
+                    
+                    
+                }
+            break;
             
+            case 4:
+                if (marcaDeLampara.equals("argentinaluz") || marcaDeLampara.equals("felipelamparas"))
+                {
+                    precioTotal=cantidadDeLamparitas*precioDeLamparita*0.75;
+                    System.out.println("debe pagar: "+precioTotal+"$.");
+                    
+                }else
+                {
+                precioTotal=cantidadDeLamparitas*precioDeLamparita*0.80;
+                    System.out.println("debe pagar: "+precioTotal+"$.");
+                }
+                
+            break;
             
-            
+            case 3:
+                if(marcaDeLampara.equals("argentinaluz"))
+                {
+                    precioTotal=cantidadDeLamparitas*precioDeLamparita*0.85;
+                    System.out.println("debe pagar: "+precioTotal+"$.");
+                }else
+                {
+                    if (marcaDeLampara.equals("felipelamparas"))
+                    {
+                      precioTotal=cantidadDeLamparitas*precioDeLamparita*0.90;
+                    System.out.println("debe pagar: "+precioTotal+"$."); 
+                    
+                    }else
+                    {
+                        precioTotal=cantidadDeLamparitas*precioDeLamparita*0.95;
+                        System.out.println("debe pagar: "+precioTotal+"$.");
+                    }
+                }
+                
+                
+            break;
             
             default:
                 
                 precioTotal=cantidadDeLamparitas*precioDeLamparita*0.50;
-                System.out.println("debe pagar: "+precioTotal);
+                System.out.println("debe pagar: "+precioTotal+"$.");
                 break;
         
         }
-            
-            
-            
     }
     
 }
