3fe5e10aa6be25c945ebb23fc86260050c560b7b
lautalb <31898586+lautalb@users.noreply.github.com>
2017-10-08 23:40:26 -0300

    primer commit
    ejercicio comentado y terminado

---
+++ EjercicioCuarentaYDos.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuarentaydos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lautalb
+ */
+public class EjercicioCuarentaYDos {
+
+    /**
+     * @param args the command line arguments
+     * 42- De un grupo de vehículos (no se sabe la cantidad) se necesita tomar el color, informar cuantos
+        rojo y cuantos azules, además del total de vehículos
+     */
+    public static void main(String[] args) {
+        //declaramos variables e importamos y creamos scanner
+        Scanner miscan = new Scanner(System.in);
+        String datoAux;
+        Integer vehiculos;
+        String color;
+        Integer rojo=0;
+        Integer azul=0;
+        Integer contador=0;
+        
+        //mostramos mensaje y pedimos datos
+        System.out.println("Ingrese cantidad de vehiculos");
+        datoAux=miscan.next();
+        vehiculos=Integer.parseInt(datoAux);
+        
+        //iteramos
+        while(contador<vehiculos)
+        {   //aumenta el contador
+            contador++;
+            
+            //mostramos mensaje y pedimos datos
+            System.out.println("Ingrese el color del auto numero: "+contador);
+            color=miscan.next();
+                  
+            //creamos la bifurcacion
+            if(color.equals("rojo"))
+            {   //si es rojo aumenta el contador de color rojo
+                rojo++;
+                
+            }else
+            {
+                if(color.equals("azul"))
+                {   //si es azul aumenta el contador azul
+                    azul++;
+                }
+            }
+        }
+        //mostramos la cantidad de vehiculos y cuantos hay de color rojo y azul.
+        System.out.println("La cantidad de vehiculos es: "+vehiculos+" la cantidad de color rojo: "+rojo+" y la cantidad de color azul: "+azul);
+    }
+    
+}
