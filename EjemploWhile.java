ddc1e1625d716dc6fcfa8e8ee719c7f348cee627
lautalb <31898586+lautalb@users.noreply.github.com>
2017-10-11 17:52:00 -0300

    segundo commit
    sacar numeros primos, contador y acumuladores, validacion de datos, salida por logica, iteracion sin cantidad, promedio con while

--- EjemploWhile.java
+++ EjemploWhile.java
@@ -23,8 +23,214 @@ public class EjemploWhile {
         //EjemploWhile.IteracionSinCantidad();
         //EjemploWhile.SalidaPorLogica();
         //EjemploWhile.PedirNumeros();
-        EjemploWhile.MostrarMenu();
+       // EjemploWhile.MostrarMenu();
+        //EjemploWhile.TomarDatos();
+        //EjemploWhile.ContadoresYAcumuladores();
+        //EjemploWhile.NumeroPrimo();
+        EjemploWhile.PrimosDelUnoAlCien();
+        
     }//finaliza el main
+    
+    public static void PrimosDelUnoAlCien()
+    {
+        Integer numeroIngresado=1;
+        
+        while(numeroIngresado<101)
+        {
+            numeroIngresado++;
+            Integer contador=2;
+            Integer contadorDeDiv=0; 
+            while(contador<numeroIngresado)
+            {  
+
+                if(numeroIngresado%contador==0)
+                {   
+                    contadorDeDiv++;
+                    break;
+                }
+                contador++;
+            }
+
+            if(contadorDeDiv>0)
+            {
+                //System.out.println("No es primo");
+            }else
+            {
+                System.out.println("Es primo el numero: "+numeroIngresado);
+            }
+        }//fin del primer while
+    }
+    
+    public static void NumeroPrimo()
+    {
+        Integer numeroIngresado=357;
+        Integer contador=2;
+        Integer contadorDeDiv=0; 
+        while(contador<numeroIngresado)
+        {
+            
+            if(numeroIngresado%contador==0)
+            {   
+                contadorDeDiv++;
+                break;
+            }
+            contador++;
+        }
+        
+        if(contadorDeDiv>0)
+        {
+            System.out.println("No es primo");
+        }else
+        {
+            System.out.println("Es primo");
+        }
+    }
+    
+    
+    
+    public static void ContadoresYAcumuladores()
+    {
+        Integer contador=0;
+        Integer acumulador=0;
+        Integer multiplicador=1;
+        Integer restador=0;
+        Integer contadorPares=0;
+        Integer contadorDivisoresDe3=0;
+        while(contador<10)
+        {
+            contador++;
+            System.out.println("Numero: "+contador);
+            acumulador=acumulador+contador;
+            multiplicador=multiplicador*contador;
+            restador=restador-contador;
+            
+            if (contador%2==0)
+            {
+                contadorPares++;
+            }
+            if(contador%3==0)
+            {
+                contadorDivisoresDe3++;
+            }
+        }
+        
+        System.out.println("la suma: "+acumulador);
+        System.out.println("la multiplicacion: "+multiplicador);
+        System.out.println("la resta: "+restador);
+        System.out.println("pares: "+contadorPares);
+        System.out.println("Cantidad de devisores x 3: "+contadorDivisoresDe3);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * Pide los datos de una persona y valida cada uno.
+     * datos a pedir:
+     * nombre
+     * edad(entre 1 y 100 años)
+     * sexo(f o m)
+     * Estado civicl(S - C - D - V)
+     * nacionalidad (N,L,E,R)
+     * Sueldo(entre 8000 y 58000)
+     * Dona organos(Si o no)
+     * clave de no menos de 6 caracteres
+     */
+    public static void TomarDatos()
+    {
+        Scanner miscan= new Scanner(System.in);
+        String datoAux;
+        String nombre;
+        Integer edad;
+        String sexo;
+        String estadoCivil;
+        String nacionalidad;
+        Integer sueldo;
+        String donador;
+        String clave;
+        
+        //pedimos datos
+        System.out.println("Ingrese nombre: ");
+        nombre=miscan.next();
+        
+        System.out.println("Ingrese clave: ");
+        clave=miscan.next();
+        while(clave.length()<6)
+        {   //el length cuenta los caracteres de la variable.
+            System.out.println("Pocos caracteres, vuelva a ingresar la clave");
+            clave=miscan.next();
+        }
+        
+        System.out.println("Ingrese edad: ");
+        datoAux=miscan.next();
+        edad=Integer.parseInt(datoAux);
+        while (edad<1 || edad>100)
+        {   //si es menor a 1 y mayor a 100 entra al while y vuelve a pedir
+            System.out.println("edad invalidad, vuelva a ingresar su edad");
+            datoAux=miscan.next();
+            edad=Integer.parseInt(datoAux);
+        }
+        
+        System.out.println("Ingrese sexo (F/M)");
+        sexo=miscan.next();
+        while (!sexo.equalsIgnoreCase("f") && !sexo.equalsIgnoreCase("m"))
+        {   //si es diferente a F y a M entra al while y vuelve a pedir datos
+            System.out.println("Sexo invalido, vuelva a ingresar su sexo");
+            sexo=miscan.next();
+        }
+        
+        System.out.println("Ingrese estado civil (S/C/D/V)");
+        estadoCivil=miscan.next();
+        while(!estadoCivil.equalsIgnoreCase("s") && !estadoCivil.equalsIgnoreCase("c") && !estadoCivil.equalsIgnoreCase("d") && !estadoCivil.equalsIgnoreCase("v"))
+        {   //si es diferente a S, C, D, V va a volver a pedir datos
+            System.out.println("Ingreso un estado civil invalido, vuelva a ingresar su estado civil.");
+            estadoCivil=miscan.next();
+        }
+        
+        System.out.println("Ingrese nacionalidad(N/L/E/R)");
+        nacionalidad=miscan.next();
+        while(!nacionalidad.equalsIgnoreCase("n") && !nacionalidad.equalsIgnoreCase("l") && !nacionalidad.equalsIgnoreCase("e") && !nacionalidad.equalsIgnoreCase("r"))
+        {   //si es diferente a N,L,E,R vuelve a pedir datos
+            System.out.println("Ingreso una nacionalidad invalidad, vuelva a ingresar su nacionalidad");
+            nacionalidad=miscan.next();
+        }        
+         
+        System.out.println("Ingrese sueldo");
+        datoAux=miscan.next();
+        sueldo=Integer.parseInt(datoAux);
+        while(sueldo<8000 || sueldo>58000)
+        {   //si es menor a 800 y mayor a 58000 vuelve a pedir
+            System.out.println("Ingreso un sueldo invalido, vuelva a ingresar sueldo");
+            datoAux=miscan.next();
+            sueldo=Integer.parseInt(datoAux);
+        }
+        
+        System.out.println("Es donador? (SI/NO)");
+        donador=miscan.next();
+        while(!donador.equalsIgnoreCase("si") && !donador.equalsIgnoreCase("no"))
+        {   //si es diferente a S y a N vuelve a pedir.
+            System.out.println("Ingreso invalido, vuelva a ingresar si es donador o no");
+            donador=miscan.next();
+        }
+        
+        //mostramos datos
+        System.out.println("Usted se llama: "+nombre);
+        System.out.println("Su clave es : "+clave);
+        System.out.println("Usted tiene: "+edad+" años de edad.");
+        System.out.println("Usted es del sexo: "+sexo);
+        System.out.println("Su estado civil es: "+estadoCivil);
+        System.out.println("Su nacionadalidad es: "+nacionalidad);
+        System.out.println("Su sueldo es: "+sueldo);
+        System.out.println("Usted "+donador+" dona.");
+    }
+    
+    
+    
+    
     
     /**
         Pide dos numeros e informa el promedio.
@@ -263,4 +469,7 @@ public class EjemploWhile {
         resultado=numeroUno/numeroDos;
         System.out.println("El resultado de la division es: "+resultado);
     }
+    
+    
+    
 }
