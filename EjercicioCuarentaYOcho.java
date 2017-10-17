eae104424b0999c6bd14b7d9b0c4284b4f7cae89
lautalb <31898586+lautalb@users.noreply.github.com>
2017-10-17 00:21:11 -0300

    primer commit
    ejercicio terminado

---
+++ EjercicioCuarentaYOcho.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuarentayocho;
+
+/**
+ *
+ * @author Lautalb
+ */
+public class EjercicioCuarentaYOcho {
+
+    /**
+     * @param args the command line arguments
+     * 48-Informar si un número es centro numérico
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
