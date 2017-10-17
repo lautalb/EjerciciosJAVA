6c0e0be26c3a470788d7a7bc2fbad475ac709880
lautalb <31898586+lautalb@users.noreply.github.com>
2017-10-17 00:21:59 -0300

    primer commit
    ejercicio terminado

---
+++ EjercicioCincuentaYDos.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocincuentaydos;
+
+/**
+ *
+ * @author Lautalb
+ */
+public class EjercicioCincuentaYDos {
+
+    /**
+     * @param args the command line arguments
+     * 52- Informar todos los números perfectos hasta el millón
+     */
+    public static void main(String[] args) {
+        Integer contador=1;
+        Integer contador2=0;
+        Integer suma=0;
+        Integer numero=2;
+        
+        while(contador2<1000000)
+        {
+            while(contador<numero-1)
+            {   
+                 
+
+                 if(numero%contador==0)
+                 {  //si es divisor se suma
+                     suma=suma+contador;
+                 }
+                 contador++;
+     
+            }
+            contador=1;
+            contador2++;
+            if(numero==suma)
+                 {
+                     System.out.println("el numero: "+numero+" es primo");
+                 }
+            suma=0;
+            numero++;
+        }
+    }
+    
+}
