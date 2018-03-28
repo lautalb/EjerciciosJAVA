bcb6dfb2170d170d75d447e498768c010b70ab9b
lautalb <31898586+lautalb@users.noreply.github.com>
2018-03-28 16:12:38 -0300

    primer commit
    ejemplo de como hacer formulario dentro de otro.

---
+++ VisorDeImagenesMDI/src/visordeimagenesmdi/VisorDeImagenesMDI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visordeimagenesmdi;
+
+import Vistas.frmInicioPrincipal;
+
+/**
+ *
+ * @author alumno
+ */
+public class VisorDeImagenesMDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmInicioPrincipal frm = new frmInicioPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
