bfe6e35f202fdd6565bff95c66e6b1cac488f1ae
lautalb <31898586+lautalb@users.noreply.github.com>
2018-03-28 16:10:45 -0300

    primer commit
    Proyecto de cine con formularios, hay que ir completandolo poco a poco.

---
+++ ProyectoCine/src/proyectocine/ProyectoCine.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocine;
+
+import Formularios.frmVistaGeneral;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProyectoCine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmVistaGeneral vista= new frmVistaGeneral();
+        vista.setVisible(true);
+    }
+    
+}
